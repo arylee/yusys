51736a680fb0a63b4178fd705612571291be2bb7
arylee <aelbery@163.com>
2020-08-17 20:12:49 +0800

    String.format() is too slow, and eat all CPU resource.

--- file-comp/src/test/java/cn/com/yusys/ary/GenFile.java
+++ file-comp/src/test/java/cn/com/yusys/ary/GenFile.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Random;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +17,20 @@ public class GenFile {
 	
 	private static String ENCODE = "UTF-8";
 	
+	private static String formatMain(int first, int second) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("CN00KEY").append(StringUtils.leftPad(String.valueOf(first), 42, "0"))
+				.append("MAIN").append(StringUtils.leftPad(String.valueOf(second), 914, "0"));
+		return builder.toString();
+	}
+	
+	private static String formatTemp(int first, int second) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("CN00KEY").append(StringUtils.leftPad(String.valueOf(first), 42, "0"))
+				.append("MAIN").append(StringUtils.leftPad(String.valueOf(second), 909, "0")).append("-TAC0");
+		return builder.toString();
+	}
+
 	public static void main(String[] args) {
 		GeneratorConfig config = GeneratorConfig.getInstance();
 		try (FileOutputStream mainStream = new FileOutputStream(config.getMainFile());				
@@ -29,25 +44,23 @@ public class GenFile {
 				int tempRowCount = 1;
 				int mainLimit = config.getMainLimit();	
 				int tempLimit = config.getTempLimit();
-				String outString = String.format("CN00KEY%042d", mainRowCount) + 
-						String.format("MAIN%0909d-TAC0", tempRowCount);
-				tempWriter.write(outString);
+				
+				//String outString = String.format("CN00KEY%042dMAIN%0909d-TAC0", mainRowCount, tempRowCount);
+				tempWriter.write(formatTemp(mainRowCount, tempRowCount));
 				tempWriter.newLine();
 				
 				while (mainRowCount < mainLimit) {
 					mainRowCount++;
 					int num = random.nextInt(10000000);
-					outString = String.format("CN00KEY%042d", mainRowCount) + 
-							String.format("MAIN%0914d", num);
-					mainWriter.write(outString);
+					//outString = String.format("CN00KEY%042dMAIN%0914d", mainRowCount, num);
+					mainWriter.write(formatMain(mainRowCount, num));
 					if (mainRowCount < mainLimit) {
 						mainWriter.newLine();
 					}
 					if (mainRowCount == (tempRowCount * mainLimit / tempLimit) && tempRowCount < tempLimit) {
 						tempRowCount++;
-						outString = String.format("CN00KEY%042d", mainRowCount) + 
-								String.format("MAIN%0909d-TAC0", tempRowCount);
-						tempWriter.write(outString);
+						//outString = String.format("CN00KEY%042dMAIN%0909d-TAC0", mainRowCount, tempRowCount);
+						tempWriter.write(formatTemp(mainRowCount, tempRowCount));
 						tempWriter.newLine();
 					}
 					if (mainRowCount % (mainLimit / 10) == 0) {
@@ -57,9 +70,8 @@ public class GenFile {
 				}
 				tempRowCount++;
 				mainRowCount++;
-				outString = String.format("CN00KEY%042d", mainRowCount) + 
-						String.format("MAIN%0909d-TAC0", tempRowCount);
-				tempWriter.write(outString);
+				//outString = String.format("CN00KEY%042dMAIN%0909d-TAC0", mainRowCount, tempRowCount);
+				tempWriter.write(formatTemp(mainRowCount, tempRowCount));
 			}
 		} catch (FileNotFoundException e) {
 			logger.error("File not found.", e);
